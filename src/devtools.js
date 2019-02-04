c03d5dffef24f34d764b3cc4e1bc3819bde4bb5d
Salvador Campanella <salvadorcampanella512@gmail.com>
2019-02-04 17:16:49 +0100

    Herramientas de desarrollo para Electron
    Herramienta para verificación del estilo de código:
    Standard https://www.npmjs.com/package/standard
    
    DevTools, nos muestra las opciones de desarrollo de la ventana
    que vamos a cargar. Electron-debug nos permite trabajar con opciones de
    desarrollo en el ambiente de desarollo.
    npm i electron-debug -S
    
    Devtron son las herramientas oficiales de Electron para hacer debug.
    npm install --save-dev devtron

--- src/devtools.js
+++ src/devtools.js
@@ -1,6 +1,10 @@
 
-import {enableLiveReload} from 'electron-compile'
+import { enableLiveReload } from 'electron-compile'
+import electronDebug from 'electron-debug'
 
-module.exports = function devtools(argument) {
-	enableLiveReload()
+module.exports = function devtools (argument) {
+  enableLiveReload()
+  electronDebug({
+  	showDevTools:true
+  })
 }
--- src/index.js
+++ src/index.js
@@ -3,37 +3,38 @@
 import { app, BrowserWindow } from 'electron'
 import devtools from './devtools'
 
-if (process.env.NODE_ENV === 'development'){
-	devtools()
+if (process.env.NODE_ENV === 'development') {
+  devtools()
 }
 
 app.on('before-quit', function () {
-	console.log('Saliendo...')
+  console.log('Saliendo...')
 })
 
-app.on('ready', function() {
-	let win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		title: 'Hola mundo',
-		center: true,
-		maximizable: false,
-		show: false
-	})
+app.on('ready', function () {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: 'Hola mundo',
+    center: true,
+    maximizable: false,
+    show: false
+  })
 
-	win.once('ready-to-show', function() {
-		win.show()
-	})
+  win.once('ready-to-show', function () {
+    win.show()
+  })
 
-	win.on('move',function() {
-		const position = win.getPosition()
-		console.log('La  posicion de la ventana es ' + position)
-	})
+  win.on('move', function () {
+    const position = win.getPosition()
+    console.log('La  posicion de la ventana es ' + position)
+  })
 
-	win.on('closed',function() {
-		win = null
-		app.quit()
-	})
+  win.on('closed', function () {
+    win = null
+    app.quit()
+  })
 
-	win.loadURL(`file://${__dirname}/renderer/index.html`)
+  win.loadURL(`file://${__dirname}/renderer/index.html`)
+  win.toggleDevTools()
 })
